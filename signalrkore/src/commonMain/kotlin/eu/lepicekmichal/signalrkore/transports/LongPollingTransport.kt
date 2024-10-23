8e2d756fbf93293c1c6d6597fd3ceb1d51971434
lepicekmichal <lepicekmichal@gmail.com>
2024-10-23 10:28:01 +0200

    missing import

--- signalrkore/src/commonMain/kotlin/eu/lepicekmichal/signalrkore/transports/LongPollingTransport.kt
+++ signalrkore/src/commonMain/kotlin/eu/lepicekmichal/signalrkore/transports/LongPollingTransport.kt
@@ -13,6 +13,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentLength
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.Flow
